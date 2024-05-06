750d594babfea097acc31f8ab5f5ddd3b2b13f9e
anjanbaradwaj <anjan.baradwaj4@gmail.com>
2024-05-06 22:36:58 +0530

    Spring Boot Security with JWT Implementation

---
+++ spring-security-jwt/src/main/java/com/mylearnings/springsecurityjwt/auth/AuthenticationService.java
@@ -0,0 +1,58 @@
+package com.mylearnings.springsecurityjwt.auth;
+
+import com.mylearnings.springsecurityjwt.config.JwtService;
+import com.mylearnings.springsecurityjwt.user.Role;
+import com.mylearnings.springsecurityjwt.user.User;
+import com.mylearnings.springsecurityjwt.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstname())
+                .lastName(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                ));
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ spring-security-jwt/src/main/java/com/mylearnings/springsecurityjwt/user/Role.java
@@ -0,0 +1,6 @@
+package com.mylearnings.springsecurityjwt.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
